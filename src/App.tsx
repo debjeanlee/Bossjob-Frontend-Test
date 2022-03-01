6ac8a637114a42c344637ad063d36693c9c45d86
betternut <debjeanlee.is@gmail.com>
2022-03-02 00:17:41 +0800

    redefine spacing vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { mockJob } from "./mocks/mockJob";
+import Divider from "./shared/components/Divider";
 import Header from "./shared/components/Header";
 import JobCard from "./shared/components/JobCard";
 import SearchSection from "./shared/components/SearchSection/SearchSection";
@@ -11,8 +12,9 @@ const App = () => {
       <div className="App-Wrapper">
         <Header />
         <SearchSection />
-        <div style={{ padding: "var(--m)" }}>
+        <div style={{ padding: "var(--l)" }}>
           <p>1231 jobs found</p>
+          <Divider style={{ marginBottom: "var(--m)" }} />
           <JobCard job={mockJob} />
         </div>
       </div>
