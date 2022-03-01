818cdbd6bce4de415e751fd613f4fd8fb5712bb4
betternut <debjeanlee.is@gmail.com>
2022-03-01 22:19:51 +0800

    add form submit handler on Input

--- src/shared/components/Input/Input.tsx
+++ src/shared/components/Input/Input.tsx
@@ -1,20 +1,31 @@
-import React, { ReactNode } from "react";
+import React, { ChangeEvent, ReactNode } from "react";
 import "./Input.css";
 
 interface InputProps {
   label?: string;
   icon?: ReactNode;
   iconPosition?: "left" | "right";
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ label, icon, iconPosition = "left" }: InputProps) => {
+const Input = ({
+  label,
+  icon,
+  iconPosition = "left",
+  onChange,
+}: InputProps) => {
   return (
     <div
       className="InputWrapper"
       style={{ flexDirection: iconPosition === "left" ? "row" : "row-reverse" }}
     >
       <div className="SearchIcon">{icon}</div>
-      <input type="text" placeholder={label} className="SearchInput" />
+      <input
+        type="text"
+        placeholder={label}
+        className="SearchInput"
+        onChange={onChange}
+      />
     </div>
   );
 };
--- src/shared/components/SearchSection/SearchSection.tsx
+++ src/shared/components/SearchSection/SearchSection.tsx
@@ -1,11 +1,25 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { ReactComponent as SearchIcon } from "../../../assets/icons/Search.svg";
+import Input from "../Input";
 import "./SearchSection.css";
 
 const SearchSection = () => {
+  const [input, setInput] = useState<string>("");
+
+  const submitForm = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log("form submitted", input);
+  };
+
   return (
     <div className="SearchSection">
-      <h3>Search for job title or company name</h3>
+      <form onSubmit={submitForm}>
+        <Input
+          label="Search for job title or company name"
+          icon={<SearchIcon />}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
