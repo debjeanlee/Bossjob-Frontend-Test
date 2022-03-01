030226911756756675107f24162303ed6cb0612d
betternut <debjeanlee.is@gmail.com>
2022-03-01 21:02:03 +0800

    create SearchSection

---
+++ src/shared/components/SearchSection/SearchSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./SearchSection.css";
+
+const SearchSection = () => {
+  return (
+    <div className="SearchSection">
+      <h3>Search for job title or company name</h3>
+    </div>
+  );
+};
+
+export default SearchSection;
