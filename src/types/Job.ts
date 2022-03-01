e2c4781c17e9a8fa2e146a59cc51434ee7398e3f
betternut <debjeanlee.is@gmail.com>
2022-03-01 22:53:10 +0800

    craeete Job interface

---
+++ src/types/Job.ts
@@ -0,0 +1,15 @@
+export interface Job {
+  id: number;
+  degree: string;
+  job_title: string;
+  job_country: string;
+  job_type: string;
+  job_location: string;
+  salary_range_from: number;
+  salary_range_to: number;
+  company_name: string;
+  company_location: string;
+  xp_lvl: string;
+  updated_at: string;
+  company_logo: string;
+}
